82eb70d0fd7263f45e19703954175105cee0ce72
GFernandesZ <g.f.z2012gfz@gmail.com>
2025-03-28 18:04:26 -0300

    Correção de codigo

--- lib/helpers/helper_taxes.dart
+++ lib/helpers/helper_taxes.dart
@@ -20,90 +20,90 @@ double calculateTaxesByAccount({
   }
 }
 
-class Transaction {
+// class Transaction {
 
-  String id;
-  String senderAccountId;
-  String receiverAccountId;
-  DateTime date;
-  double amount;
-  double taxes;
+//   String id;
+//   String senderAccountId;
+//   String receiverAccountId;
+//   DateTime date;
+//   double amount;
+//   double taxes;
 
-  Transaction({
-    required this.id,
-    required this.senderAccountId,
-    required this.receiverAccountId,
-    required this.date,
-    required this.amount,
-    required this.taxes,
-  });
+//   Transaction({
+//     required this.id,
+//     required this.senderAccountId,
+//     required this.receiverAccountId,
+//     required this.date,
+//     required this.amount,
+//     required this.taxes,
+//   });
 
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'id': id,
-      'senderAccountId': senderAccountId,
-      'receiverAccountId': receiverAccountId,
-      'date': date.millisecondsSinceEpoch,
-      'amount': amount,
-      'taxes': taxes,
-    };
-  }
+//   Map<String, dynamic> toMap() {
+//     return <String, dynamic>{
+//       'id': id,
+//       'senderAccountId': senderAccountId,
+//       'receiverAccountId': receiverAccountId,
+//       'date': date.millisecondsSinceEpoch,
+//       'amount': amount,
+//       'taxes': taxes,
+//     };
+//   }
 
-  factory Transaction.fromMap(Map<String, dynamic> map) {
-    return Transaction(
-      id: map['id'] as String,
-      senderAccountId: map['senderAccountId'] as String,
-      receiverAccountId: map['receiverAccountId'] as String,
-      date: DateTime.fromMillisecondsSinceEpoch(map['date'] as int),
-      amount: map['amount'] as double,
-      taxes: map['taxes'] as double,
-    );  
-  }
+//   factory Transaction.fromMap(Map<String, dynamic> map) {
+//     return Transaction(
+//       id: map['id'] as String,
+//       senderAccountId: map['senderAccountId'] as String,
+//       receiverAccountId: map['receiverAccountId'] as String,
+//       date: DateTime.fromMillisecondsSinceEpoch(map['date'] as int),
+//       amount: map['amount'] as double,
+//       taxes: map['taxes'] as double,
+//     );  
+//   }
 
-  Transaction copyWith({
-    String? id,
-    String? senderAccountId,
-    String? receiverAccountId,
-    DateTime? date,
-    double? amount,
-    double? taxes,
-  }) {
-    return Transaction(
-      id: id ?? this.id,
-      senderAccountId: senderAccountId ?? this.senderAccountId,
-      receiverAccountId: receiverAccountId ?? this.receiverAccountId,
-      date: date ?? this.date,
-      amount: amount ?? this.amount,
-      taxes: taxes ?? this.taxes,
-    );
-  }
+//   Transaction copyWith({
+//     String? id,
+//     String? senderAccountId,
+//     String? receiverAccountId,
+//     DateTime? date,
+//     double? amount,
+//     double? taxes,
+//   }) {
+//     return Transaction(
+//       id: id ?? this.id,
+//       senderAccountId: senderAccountId ?? this.senderAccountId,
+//       receiverAccountId: receiverAccountId ?? this.receiverAccountId,
+//       date: date ?? this.date,
+//       amount: amount ?? this.amount,
+//       taxes: taxes ?? this.taxes,
+//     );
+//   }
   
-  @override
-  String toString() {
-    return '\\nTransação $id\\n Enviado por$senderAccountId\\nRecebido por: $receiverAccountId\\nHora: $date\\nQuantidade: $amount\\nTaxas: $taxes';
-  }
+//   @override
+//   String toString() {
+//     return '\\nTransação $id\\n Enviado por$senderAccountId\\nRecebido por: $receiverAccountId\\nHora: $date\\nQuantidade: $amount\\nTaxas: $taxes';
+//   }
 
-  //O método toString fornece uma representação textual de um objeto, útil para debugging.
-  @override
-  bool operator ==(covariant Transaction other) {
-    if (identical(this, other)) return true;
+//   //O método toString fornece uma representação textual de um objeto, útil para debugging.
+//   @override
+//   bool operator ==(covariant Transaction other) {
+//     if (identical(this, other)) return true;
 
-    return other.id == id &&
-        other.senderAccountId == senderAccountId &&
-        other.receiverAccountId == receiverAccountId &&
-        other.date == date &&
-        other.amount == amount &&
-        other.taxes == taxes;
-  }
+//     return other.id == id &&
+//         other.senderAccountId == senderAccountId &&
+//         other.receiverAccountId == receiverAccountId &&
+//         other.date == date &&
+//         other.amount == amount &&
+//         other.taxes == taxes;
+//   }
 
-  @override
-  int get hashCode {
-    return id.hashCode ^
-        senderAccountId.hashCode ^
-        receiverAccountId.hashCode ^
-        date.hashCode ^
-        amount.hashCode ^
-        taxes.hashCode;
-  }
-  //O operador == e o método hashCode são fundamentais para a comparação de objetos. O Dart os usa para determinar se duas instâncias de uma classe são iguais.
-}
+//   @override
+//   int get hashCode {
+//     return id.hashCode ^
+//         senderAccountId.hashCode ^
+//         receiverAccountId.hashCode ^
+//         date.hashCode ^
+//         amount.hashCode ^
+//         taxes.hashCode;
+//   }
+//   //O operador == e o método hashCode são fundamentais para a comparação de objetos. O Dart os usa para determinar se duas instâncias de uma classe são iguais.
+// }
---
+++ lib/services/transaction_service.dart
@@ -0,0 +1,11 @@
+import 'package:dart_assincronismo/models/accounts.dart';
+
+class TransactionService {
+  makeTransaction({
+    required String idSender,
+    required String idReceive,
+    required double amount,
+  }) async  {
+    List<Account> listAccount = ;
+  }
+}
